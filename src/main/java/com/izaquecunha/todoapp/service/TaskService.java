4944a9f183dc7ca8fac94b8d4c472c39bfb205ef
izaquemacielcunha <izaquemaciel@hotmail.com>
2024-12-07 19:06:20 -0300

    Update taskservice whith create and getall methods

--- src/main/java/com/izaquecunha/todoapp/service/TaskService.java
+++ src/main/java/com/izaquecunha/todoapp/service/TaskService.java
@@ -1,6 +1,11 @@
 package com.izaquecunha.todoapp.service;
 
+import java.util.List;
+
+import com.izaquecunha.todoapp.dto.TaskDto;
 
 public interface TaskService {
+	void create(TaskDto taskDto);
+	List<TaskDto> getAll();
 
 }// end of interface
--- src/main/java/com/izaquecunha/todoapp/service/impl/TaskServiceImpl.java
+++ src/main/java/com/izaquecunha/todoapp/service/impl/TaskServiceImpl.java
@@ -1,7 +1,13 @@
 package com.izaquecunha.todoapp.service.impl;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
+import com.izaquecunha.todoapp.dto.TaskDto;
+import com.izaquecunha.todoapp.entity.Task;
+import com.izaquecunha.todoapp.mapper.TaskMapper;
 import com.izaquecunha.todoapp.repository.TaskRepository;
 import com.izaquecunha.todoapp.service.TaskService;
 
@@ -11,5 +17,17 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class TaskServiceImpl implements TaskService {
 	private TaskRepository taskRepository;
+
+	@Override
+	public void create(TaskDto taskDto) {
+		taskDto.setStatus("To Do");
+		Task task = TaskMapper.mapToTask(taskDto);
+		taskRepository.save(task);
+	}
+
+	@Override
+	public List<TaskDto> getAll() {
+		return taskRepository.findAll().stream().map(task -> TaskMapper.mapToTaskDto(task)).collect(Collectors.toList());
+	}
 
 }// end of class
